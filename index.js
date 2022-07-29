120690a43e47c423fd3dd5f4c60afa058d6320b8
DanielOhn <ohndaniel@gmail.com>
2022-07-29 15:20:44 -0700

    Ready for deployment

--- index.js
+++ index.js
@@ -5,19 +5,14 @@ const pool = require("./db");
 
 const PORT = process.env.PORT || 5002;
 const path = require('path');
-//process.env.PORT
-// process.env.NODE_ENV => productin or undefined
-
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-    // server static content
-    // npm run build
+
     app.use(express.static(path.join(__dirname, "client/build")));
-    //app.use(express.static("./client/build"));
 }
 
 // Routes
